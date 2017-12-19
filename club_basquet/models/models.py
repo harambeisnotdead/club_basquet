0c8fa07f8b4539a76b8efc9b58c9d7644c224f3a
harambeisnotdead <harambeisnotdead@users.noreply.github.com>
2017-12-19 13:22:44 +0100

    Comentarios

--- club_basquet/models/models.py
+++ club_basquet/models/models.py
@@ -2,31 +2,34 @@
 
 from openerp import models, fields, api
 
-# class club_basquet(models.Model):
-#     _name = 'club_basquet.club_basquet'
-#     name = fields.Char(string="Nom", required=True)
-#     description = fields.Text()
-
 class club_basquet_equip(models.Model):
+    """
+        Classe equip amb els atributs nom(Char), color(Char) i patrocinador(Char)
+    """
     _name = "club_basquet.equip"
     nom = fields.Char(string="Nom", required=True)
     color = fields.Char(string="Color")
     patrocinador = fields.Char(string="Patrocinador")
 
 class club_basquet_entrenador(models.Model):
+    """
+        Classe entrenador amb els atributs nom(Char) i dni(Char)
+    """
     _name = "club_basquet.entrenador"
     nom = fields.Char(string="Nom")
     dni = fields.Char(string="DNI", required=True, size=9)
 
 class club_basquet_jugador(models.Model):
+    """
+        Classe jugador amb els atributs nom(Char) i dni(Char)
+    """
     _name = "club_basquet.jugador"
     nom = fields.Char(string="Nom")
     dni = fields.Char(string="DNI", required=True, size=9)
 
 class club_basquet_caricatura(models.Model):
+    """
+        Classe caricatura amb l'atribut descripcio(Text)
+    """
     _name = "club_basquet.caricatura"
     descripcio = fields.Text(string="Descripció")
-
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
